4704b252bda55e24dcd91a479ec37dd0886b0432
Min <minsun@stuffofminsun.com>
2025-01-01 11:44:32 -0500

    chore: formatting setup

--- problemset/euler.hpp
+++ problemset/euler.hpp
@@ -1,38 +1,38 @@
 #include <chrono>
-#include <iostream>
-#include <vector>
 #include <cmath>
+#include <iostream>
 #include <string>
 #include <unordered_map>
-#include <chrono>
+#include <vector>
 
 namespace euler {
 
 bool is_prime(long);
 
 /**
-* @brief pretty prints unordered maps
-* @tparam K key of pair
-* @tparam V value of pair
-* @param unordered map to print
-*/
-template<typename K, typename V>
+ * @brief pretty prints unordered maps
+ * @tparam K key of pair
+ * @tparam V value of pair
+ * @param unordered map to print
+ */
+template <typename K, typename V>
 void print(const std::unordered_map<K, V>& kv) {
-    std::cout << "{ ";
-    for (const auto& i: kv) {
-        std::cout << i.first << ": " << i.second << " ";
-    }
-    std::cout << "}\n";
+  std::cout << "{ ";
+  for (const auto& i : kv) {
+    std::cout << i.first << ": " << i.second << " ";
+  }
+  std::cout << "}\n";
 }
 
 // todo - refactor later because think this timing is a bit bad but good enough
 // for a proxy for now
-template<typename F, typename... Args>
+template <typename F, typename... Args>
 long long int ntime(F func, Args&&... args) {
-    auto start = std::chrono::high_resolution_clock::now();
-    func(std::forward<Args>(args)...);
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+  auto start = std::chrono::high_resolution_clock::now();
+  func(std::forward<Args>(args)...);
+  auto end = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+      .count();
 }
 
-}
+}  // namespace euler
