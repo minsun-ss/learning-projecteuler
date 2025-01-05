f6a2f9a25776f2f15a1c7ac2e25a4a3166aef07b
Min <minsun@stuffofminsun.com>
2025-01-05 13:09:21 -0500

    fix: updated problem 3 to include time

--- problemset/0003.cpp
+++ problemset/0003.cpp
@@ -1,24 +1,16 @@
 #include "euler.hpp"
 
 using namespace std;
+using namespace euler;
 
 // The prime factors of 13195 are 5, 7, 13, 29
 // What is the largest prime factor of the number 600851475143?
-
-bool is_prime(long num) {
-  for (int i = 2; i <= int(pow(num, 0.5)); i++) {
-    if (num % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
 
 int largest_factor(long num) {
   std::vector<int> factors;
 
   for (int i = 2; i <= int(pow(num, 0.5)); i++) {
-    if ((num % i == 0) && is_prime(i)) {
+    if ((num % i == 0) && euler::is_prime(i)) {
       factors.push_back(i);
     }
   }
@@ -30,7 +22,9 @@ int largest_factor(long num) {
 }
 
 int main() {
-  auto val = largest_factor(600851475143);
-  cout << "Largest prime factor: " << val << "\n";
-  return 0;
+    auto [result, duration] = euler::ntime(largest_factor, 600851475143);
+    cout << "Largest prime factor: " << result << "\n";
+    cout.imbue(locale(""));
+    cout << "Duration (ns): " << duration << "\n";
+    return 0;
 }
